6abb6120ae1fc75d91cdfe03d38a57bf4df7a033
RuslanGitelman <47743960+RuslanGitelman@users.noreply.github.com>
2021-03-18 08:51:18 +0300

    - add arguments to log

--- utils/logger.aspect.ts
+++ utils/logger.aspect.ts
@@ -1,6 +1,7 @@
 import {beforeMethod, afterMethod, Metadata} from "aspect.js";
 import allure from "@wdio/allure-reporter";
 import log from '@wdio/logger'
+
 const logger = log('test')
 
 export default class LoggerAspect {
@@ -8,9 +9,16 @@ export default class LoggerAspect {
         methodNamePattern: /^/
     })
     invokeBeforeMethod(meta: Metadata) {
-        logger.info(meta.method.name.toString())
-        allure.startStep(meta.method.name.toString())
+        let res = meta.method.name.toString()
+            .replace(/([A-Z])/g, ' $1')
+            .toLowerCase()
+        meta.method.args.forEach((arg) => {
+            res += ` "${arg}"`
+        })
+        logger.info(res)
+        allure.startStep(res)
     }
+
     @afterMethod({
         methodNamePattern: /^/
     })
